d7e42f9c7f2541677c49daac2477db9c8a4042c2
jaylen ross <62577721+jaylenross@users.noreply.github.com>
2020-11-23 16:38:19 -0500

    edits

--- script.js
+++ script.js
@@ -10,38 +10,65 @@ $(function () {
         })
         .setClassToggle("#letterclosed", "show") // add class toggle
         .addTo(controller);
-    new ScrollMagic.Scene({
-            triggerElement: "#one",
-            duration: "600px", // the scene should last for a scroll distance of 400px
-            offset: 50 // start this scene after scrolling for 100px
-        })
-        .setClassToggle("#letterclosed", "hide") // add class toggle
-        .addTo(controller);
-    
- new ScrollMagic.Scene({
-            triggerElement: "#one",
-            duration: "600px", // the scene should last for a scroll distance of 400px
-            offset: 50 // start this scene after scrolling for 100px
-        })
-        .setClassToggle("#letter", "show") // add class toggle
-        .addTo(controller);
-    
-    new ScrollMagic.Scene({
-            triggerElement: "#two",
-            duration: "400%", // the scene should last for a scroll distance of 400% of the height, this will get it to stay in the position
-            offset: 100 // start this scene after scrolling for 100px
-        })
-        .setClassToggle("#day", "show") // add class toggle
-        .addTo(controller);
-    
 
+
+        new ScrollMagic.Scene({
+                triggerElement: "#one",
+                duration: "400%", // the scene should last for a scroll distance of 400px
+                offset: 50 // start this scene after scrolling for 100px
+            })
+            .setClassToggle("#letter", "showtwo") // add class toggle
+            .addTo(controller);
+        
+    
+    
     new ScrollMagic.Scene({
-            triggerElement: "#three",
-            duration: 400, // the scene should last for a scroll distance of 500px
-            offset: 100 // start this scene after scrolling for 100px
-        })
-        .setClassToggle("#moon", "show") // add class toggle
-        .addTo(controller);
+                triggerElement: "#two",
+                duration: "400%", // the scene should last for a scroll distance of 400px
+                offset: 50 // start this scene after scrolling for 100px
+            })
+            .setClassToggle(".reel", "showtwo") // add class toggle
+            .addTo(controller);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // new ScrollMagic.Scene({
+    //            triggerElement: "#one",
+    //            duration: "600px", // the scene should last for a scroll distance of 400px
+    //            offset: 50 // start this scene after scrolling for 100px
+    //        })
+    //        .setClassToggle("#letter", "show") // add class toggle
+    //        .addTo(controller);
+    //    
+    //    new ScrollMagic.Scene({
+    //            triggerElement: "#two",
+    //            duration: "400%", // the scene should last for a scroll distance of 400% of the height, this will get it to stay in the position
+    //            offset: 100 // start this scene after scrolling for 100px
+    //        })
+    //        .setClassToggle("#day", "show") // add class toggle
+    //        .addTo(controller);
+    //    
+    //
+    //    new ScrollMagic.Scene({
+    //            triggerElement: "#three",
+    //            duration: 400, // the scene should last for a scroll distance of 500px
+    //            offset: 100 // start this scene after scrolling for 100px
+    //        })
+    //        .setClassToggle("#moon", "show") // add class toggle
+    //        .addTo(controller);
 
 
 });
